db1bcae2f359babcb07fffb7694e7c459de0d228
ali-yar <32914277+ali-yar@users.noreply.github.com>
2018-10-12 01:34:13 +0200

    changes

--- Labs/Lab 1/registrationLab/affineReg2D.m
+++ Labs/Lab 1/registrationLab/affineReg2D.m
@@ -1,5 +1,6 @@
 function [ Iregistered, M] = affineReg2D( Imoving, Ifixed )
-%Example of 2D affine registration
+
+% Example of 2D affine registration
 %   Robert Martí  (robert.marti@udg.edu)
 %   Based on the files from  D.Kroon University of Twente 
 
@@ -7,14 +8,19 @@ function [ Iregistered, M] = affineReg2D( Imoving, Ifixed )
 clear all; close all; clc;
 
 % Read two images 
-Imoving=im2double(rgb2gray(imread('brain1.png'))); 
-Ifixed=im2double(rgb2gray(imread('brain2.png')));
+Imoving = im2double(rgb2gray(imread('brain1.png'))); 
+Ifixed = im2double(rgb2gray(imread('brain2.png')));
 
-Im=Imoving;
-If=Ifixed;
+Im = Imoving;
+If = Ifixed;
 
-mtype = 'sd'; % metric type: sd: ssd gcc: gradient correlation; cc: cross-correlation
-ttype = 'a'; % rigid registration, options: r: rigid, a: affine
+% metric type
+% sd: ssd | gcc: gradient correlation | cc: cross-correlation
+mtype = 'sd';
+
+% rigid registration
+% r: rigid, a1: affine (6-params), a2: affine (7-params)
+ttype = 'a2'; 
 
 % Parameter scaling of the Translation and Rotation
 % and initial parameters
@@ -22,18 +28,24 @@ switch ttype
     case 'r'
         x=[0 0 0];
         scale = [1 1 0.1];
-    case 'a'
+    case 'a1'
         x=[0 0 1 0 0 1];
-        scale = [1 1 0.1 0.1 0.1 0.1];
-         
+        scale = [1 1 0.001 0.001 0.001 0.001];
+    case 'a2'
+        x =     [0  0   0     1   1     0      0];
+        scale = [1  1 0.01 0.1 0.1 0.0001 0.0001];
 end
 
 x = x ./ scale;
-    
+
 % optimizer  
+opts = optimset('Display','iter',...
+                'MaxIter',5000,...
+                'TolFun', 1.0e-15,...
+                'TolX', 1.0e-15,...
+                'MaxFunEvals', 10000*length(x));
 [x] = fminsearch(@(x)affine_registration_function(x,scale,Im,If,mtype,ttype),...
-    x,optimset('Display','iter','MaxIter',10000, 'TolFun', 1.000000e-15,'TolX',...
-    1.000000e-15, 'MaxFunEvals', 10000*length(x)));
+    x,opts);
 
 % [x]=fminsearch(@(x)affine_registration_function(x,scale,Im,If,mtype,ttype),x,optimset('Display','iter','MaxIter',1000, 'TolFun', 1.000000e-10,'TolX', 1.000000e-10, 'MaxFunEvals', 1000*length(x),'PlotFcns',@optimplotfval));
 
@@ -45,10 +57,27 @@ switch ttype
         M = [ cos(x(3)) sin(x(3)) x(1);
              -sin(x(3)) cos(x(3)) x(2);
                0           0      1 ];
-	case 'a'
+	case 'a1'
         M = [ x(3)  x(4)    x(1);
-             x(5)  x(6)	x(2);
+              x(5)  x(6)	x(2);
               0       0 	1  ];
+    case 'a2'
+        Rot = [ cos(x(3)) sin(x(3)) 0;
+               -sin(x(3)) cos(x(3)) 0;
+                 0           0      1 ];
+        Transl = [ 1    0   x(1);
+                  0    1   x(2);
+                  0    0    1 ];
+        Scale = [  x(4)  0    0;
+                   0     x(5)  0;
+                   0     0     1 ];
+        ShearX = [ 1    x(6)   0;
+                   0     1     0;
+                   0     0     1 ];
+        ShearY = [ 1     0     0;
+                   x(7)  1     0;
+                   0     0     1 ];
+        M = Rot * Transl * Scale * ShearX * ShearY;
 end
      
 
@@ -61,5 +90,6 @@ subplot(2,2,1), imshow(If);
 subplot(2,2,2), imshow(Im);
 subplot(2,2,3), imshow(Icor);
 subplot(2,2,4), imshow(abs(If-Icor));
+
 end
 
